7516d7273741876ed8124ed1d7b5802e891293a8
mpadge <mark.padgham@email.com>
2024-03-14 11:16:06 +0100

    reinstate all tests

--- tests/testthat/test-editors.R
+++ tests/testthat/test-editors.R
@@ -10,14 +10,14 @@ test_that ("editor names", {
     expect_type (editors, "character")
     expect_true (length (editors) > 10L)
 
-    # q <- gh_editors_team_qry (stats = TRUE)
-    # editors_stats <- gh::gh_gql (query = q)
-    # editors_stats <- editors_stats$data$organization$team$members$nodes
-    # expect_true (length (editors_stats) > 5L)
-    # expect_true (length (editors_stats) < length (editors))
-    # nms <- unique (unlist (lapply (editors_stats, names)))
-    # expect_identical (nms, "login") # should have 'login' only
-    # editors_stats <- vapply (editors_stats, function (i) i$login, character (1L))
-    # expect_type (editors_stats, "character")
-    # expect_true (length (editors_stats) > 5L)
+    q <- gh_editors_team_qry (stats = TRUE)
+    editors_stats <- gh::gh_gql (query = q)
+    editors_stats <- editors_stats$data$organization$team$members$nodes
+    expect_true (length (editors_stats) > 5L)
+    expect_true (length (editors_stats) < length (editors))
+    nms <- unique (unlist (lapply (editors_stats, names)))
+    expect_identical (nms, "login") # should have 'login' only
+    editors_stats <- vapply (editors_stats, function (i) i$login, character (1L))
+    expect_type (editors_stats, "character")
+    expect_true (length (editors_stats) > 5L)
 })
