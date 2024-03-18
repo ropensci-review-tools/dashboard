227cdeaeec0b986a46e95dbc73f721c8dac9ffc7
mpadge <mark.padgham@email.com>
2024-03-18 16:56:53 +0100

    fix all calls to pkg internal fn 'add_gt_html'

--- R/gt-table.R
+++ R/gt-table.R
@@ -20,21 +20,9 @@ open_gt_table <- function (dat) {
     requireNamespace ("gt")
     u <- "https://github.com/ropensci/software-review/issues/"
     dat_url <- dat
-    add_html <- function (dat_url, what = "number") {
-        dat_url [[what]] <- paste0 (
-            "<p><a href=",
-            u,
-            dat_url$number,
-            ">",
-            dat_url [[what]],
-            "</a>"
-        )
-        dat_url [[what]] <- lapply (dat_url [[what]], gt::html)
-        return (dat_url)
-    }
     # Number has to come last here!!
-    dat_url <- add_html (dat_url, "title")
-    dat_url <- add_html (dat_url, "number")
+    dat_url <- add_gt_html (dat_url, "title")
+    dat_url <- add_gt_html (dat_url, "number")
     dat_url$stage_elapsed <- Sys.Date () - dat_url$stage_date
 
     # Then create an "urgency" column used to highlight rows needing urgent
