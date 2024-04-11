866501a3e4bb43c0fd3f651ccffeb7b8bbb27358
mpadge <mark.padgham@email.com>
2024-04-11 12:19:54 +0200

    rearrange 'editor_vacation_status' fn for #24

--- R/editors-airtable.R
+++ R/editors-airtable.R
@@ -5,24 +5,37 @@
 #' @export
 editor_vacation_status <- function () {
 
+    edvac_airtable <- edvac_status_airtable ()
+
+    edvac <- edvac_airtable
+
+    return (edvac)
+}
+
+edvac_status_airtable <- function () {
     editor_vacation <- airtabler::airtable (
         base = "app8dssb6a7PG6Vwj", table = "editor-vacation-status"
     )
     fields <- list ("editor", "Action", "Start Date", "Return Date")
-    editor_vacation <- editor_vacation$`editor-vacation-status`$select_all (fields = fields)
-    index <- vapply (editor_vacation$editor, function (i) length (i) > 0L, logical (1L))
+    editor_vacation <-
+        editor_vacation$`editor-vacation-status`$select_all (fields = fields)
+    index <- vapply (
+        editor_vacation$editor,
+        function (i) length (i) > 0L,
+        logical (1L)
+    )
     edvac <- editor_vacation [which (index), ]
 
     rev_prod <- airtabler::airtable (
         base = "app8dssb6a7PG6Vwj", table = "reviewers-prod"
     )
-    rev_prod <- rev_prod$`reviewers-prod`$select_all (fields = list ("github", "name"))
+    rev_prod <-
+        rev_prod$`reviewers-prod`$select_all (fields = list ("github", "name"))
     rev_prod <- rev_prod [match (edvac$editor, rev_prod$id), ]
     edvac$github <- rev_prod$github
     edvac$name <- rev_prod$name
 
     edvac$away <- FALSE
-    s <- edvac$`Start Date` [2]
     index <- (!is.na (edvac$`Start Date`) & is.na (edvac$`Return Date`)) |
         as.Date (edvac$`Return Date`) > Sys.Date ()
     index [which (is.na (index))] <- FALSE
