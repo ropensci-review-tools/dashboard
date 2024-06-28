a8d2416c3c8262adaab20e9404a53912b7c57a3b
mpadge <mark.padgham@email.com>
2024-06-28 12:42:10 +0200

    fix tests: now only 5 stats editors

--- tests/testthat/test-editors.R
+++ tests/testthat/test-editors.R
@@ -13,11 +13,11 @@ test_that ("editor names", {
     q <- gh_editors_team_qry (stats = TRUE)
     editors_stats <- gh::gh_gql (query = q)
     editors_stats <- editors_stats$data$organization$team$members$nodes
-    expect_true (length (editors_stats) > 5L)
+    expect_true (length (editors_stats) >= 5L)
     expect_true (length (editors_stats) < length (editors))
     nms <- unique (unlist (lapply (editors_stats, names)))
     expect_identical (nms, "login") # should have 'login' only
     editors_stats <- vapply (editors_stats, function (i) i$login, character (1L))
     expect_type (editors_stats, "character")
-    expect_true (length (editors_stats) > 5L)
+    expect_true (length (editors_stats) >= 5L)
 })
